de87d80596cbeca3fcd28c443c5d01758b009669
rmoisesjr <84817685+rmoisesjr@users.noreply.github.com>
2024-04-03 13:17:51 -0400

    Update ip,scanner.sh

--- ip,scanner.sh
+++ ip,scanner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for ip in $(seq 1 254);do
-ping -c 1 192.168.1.$ip | grep "64 bytes" | cut -d ' ' -f 4 | tr -d ':' >> ip.scanner.txt 
+for ip in $(seq 1 255);do
+ping -c 1 192.168.1.$ip | grep "bytes from" | cut -d " " -f 4 | cut -d ':' 1 2>/dev/null >> ip.scanner.txt &
 done
